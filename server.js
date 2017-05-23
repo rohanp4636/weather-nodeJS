5a98f8ea32dac0065db7356d21f88fe3f21e6a96
Rohan Patel <rohanp4636@gmail.com>
2017-05-23 17:13:22 -0400

    modified to deploy on heroku

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const axios = require('axios');
 
 var express = require('express');
 
-var app = express();
+var cors = require('cors')
+
+var app = express()
+app.use(cors());
 
 
 const port = process.env.PORT || 3000;
@@ -11,7 +14,7 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
 
-  var address = "42 harte place";
+  var address = req.query.address;
 
   var encodedAddress = encodeURIComponent(address);
   var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`
@@ -19,12 +22,13 @@ app.get('/', (req, res) => {
     if(response.data.status === 'ZERO_RESULTS'){
       throw new Error('Unable to find that address.');
     }
-
+    address = response.data.results[0].formatted_address;
     var lat = response.data.results[0].geometry.location.lat;
     var long = response.data.results[0].geometry.location.lng;
     var weatherUrl = `https://api.darksky.net/forecast/e4f4de4ba5614a6a3f3b41177e1f818b/${lat},${long}`;
 
     return axios.get(weatherUrl).then( (response) => {
+
       var temperature = response.data.currently.temperature;
       var apparentTemperature = response.data.currently.apparentTemperature;
       res.send( {
@@ -36,7 +40,6 @@ app.get('/', (req, res) => {
     });
 
   }).catch( (error) => {
-    console.log(error);
     res.send('Ooops. Something went wrong.');
 
   });
